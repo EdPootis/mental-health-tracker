5edb0a41f064712f8bb5dff804d06f7fffb0fabe
Edmond Christian <edmond.chr@gmail.com>
2024-09-18 15:56:54 +0700

    Tutorial 3: Added users

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%hk21kqdcvo^altp6f7a@20ce@h01tq9nlyz@ub3^k0w72)06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "edmond-christian31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
